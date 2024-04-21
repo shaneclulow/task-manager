53031e3d317c467c8916abfef1a3299182864d66
shaneclulow <s.d.clulow@gmail.com>
2024-04-21 11:54:24 +0100

    Updates

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
